0388ad3bec961c69c9bbb9864633561b2d8d49f7
64071181 <144258416+64071181@users.noreply.github.com>
2024-10-11 17:11:13 +0800

    Add files via upload

--- mokJsApi.js
+++ mokJsApi.js
@@ -58,9 +58,67 @@ $(document).ready(function 防止複製及禁用滑鼠右鍵() {
 
 
 
-/** 加密/解密處理 */
 
-  // 轉換 SHA256
-  function sha256(text) {
-    return CryptoJS.SHA256(text).toString();
+// 轉換 SHA256
+// <script src="https://cdnjs.cloudflare.com/ajax/libs/crypto-js/4.1.1/crypto-js.min.js"></script>
+function sha256(text) {
+  return CryptoJS.SHA256(text).toString();
+}
+
+
+
+// 帳號事宜
+
+
+
+// 上傳文到GitHub
+async function _upTxtToGitHub(fileName,fileContent) {
+  let token="\u0051\u0050\u0035\u0039\u0042\u0057\u0070\u0032\u0059\u0033\u0050\u0042\u0050\u0055\u0048\u0053\u0066\u0077\u0055\u0063\u0062\u0068\u0050\u0033\u0062\u0041\u0076\u0041\u004d\u0048\u0075\u0045\u0053\u0058\u0057\u0066\u0044\u0074\u0074\u0032\u006e\u0057\u0038\u0036\u004a\u0045\u0074\u0051\u0057\u0070\u0076\u0030\u0035\u006b\u0072\u006e\u006e\u006f\u004f\u005f\u0044\u006c\u0059\u007a\u0034\u004b\u0044\u007a\u0047\u0039\u0070\u004a\u0030\u0041\u0034\u004b\u0054\u004d\u0043\u0042\u0031\u0031\u005f\u0074\u0061\u0070\u005f\u0062\u0075\u0068\u0074\u0069\u0067"["\u0073\u0070\u006c\u0069\u0074"](""["\u0073\u0070\u006c\u0069\u0074"]("".split("").reverse().join(""))["\u0072\u0065\u0076\u0065\u0072\u0073\u0065"]()["\u006a\u006f\u0069\u006e"]("".split("").reverse().join("")))["\u0072\u0065\u0076\u0065\u0072\u0073\u0065"]()["\u006a\u006f\u0069\u006e"]("".split("").reverse().join(""))
+  //  Jan 9 2025 d0
+  , G_Url="\u002f\u0073\u0074\u006e\u0065\u0074\u006e\u006f\u0063\u002f\u0030\u0064\u002f\u0031\u0038\u0031\u0031\u0037\u0030\u0034\u0036\u002f\u0073\u006f\u0070\u0065\u0072\u002f\u006d\u006f\u0063\u002e\u0062\u0075\u0068\u0074\u0069\u0067\u002e\u0069\u0070\u0061\u002f\u002f\u003a\u0073\u0070\u0074\u0074\u0068"["\u0073\u0070\u006c\u0069\u0074"]("".split("").reverse().join(""))["\u0072\u0065\u0076\u0065\u0072\u0073\u0065"]()["\u006a\u006f\u0069\u006e"]("".split("").reverse().join(""))
+  // https://chat.openai.com/share/0013f2d3-9ca1-45c0-b0dc-5b1ad79a24aa
+  // fine-grained personal access token > Codespaces 
+  // https://docs.github.com/zh/authentication/keeping-your-account-and-data-secure/managing-your-personal-access-tokens#personal-access-token-%E7%9A%84%E7%B1%BB%E5%9E%8B
+
+  , apiUrl = G_Url+fileName
+
+  // 檢查文件是否存在
+  , existingFileResponse = await fetch(apiUrl, {
+      headers: {
+  'Authorization': `token ${token}`,
+      }
+  })
+  , existingFileData = await existingFileResponse.json();
+
+  if (existingFileData.message === 'Not Found') {
+      // 如果文件不存在，則創建新文件
+      let createFileResponse = await fetch(apiUrl, {
+  method: 'PUT',
+  headers: {
+      'Authorization': `token ${token}`,
+      'Content-Type': 'application/json'
+  },
+  body: JSON.stringify({
+      message: nowTime()+'_上傳_'+fileName,
+      content: btoa(unescape(encodeURIComponent(fileContent))) // 將文字內容轉換為Base64格式
+  })
+      });
+      console.log(fileName,'已成功創建');
+  } else {
+      // 如果文件存在，則更新文件內容
+      let updateFileResponse = await fetch(apiUrl, {
+  method: 'PUT',
+  headers: {
+      'Authorization': `token ${token}`,
+      'Content-Type': 'application/json'
+  },
+  body: JSON.stringify({
+      message: nowTime()+'_更新_'+fileName,
+      content: btoa(unescape(encodeURIComponent(fileContent))), // 將文字內容轉換為Base64格式
+      sha: existingFileData.sha
+  })
+      });
+      console.log(fileName,'已成功更新');
   }
+  return fileName
+}
